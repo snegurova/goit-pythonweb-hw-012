44dd047d941a2f31fe94d5e1810face7de4c47e7
innasnegurova <innasnegurova@nxte.nl>
2025-05-03 22:46:09 +0200

    feat: add CORS origins

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, Request, status
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 from slowapi.errors import RateLimitExceeded
 
 from src.api import contacts, utils, auth, users
 
 app = FastAPI()
+
+origins = [
+    "<http://localhost:3000>"
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(RateLimitExceeded)
 async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
