655f1a541bd7f4cbe42ae42264c297b13a038f08
innasnegurova <innasnegurova@nxte.nl>
2025-05-11 23:04:51 +0200

    feat: set redis cloud db

--- src/cache/redis_client.py
+++ src/cache/redis_client.py
@@ -1,9 +1,4 @@
 import redis
 import os
 
-redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST", "localhost"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
-    db=0,
-    decode_responses=True
-)
+redis_client = redis.Redis.from_url(os.getenv("REDIS_URL"))
